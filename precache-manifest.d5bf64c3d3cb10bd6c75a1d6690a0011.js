a929e2317a534f45a0af7370ab8575f15697cda4
Guerin Yves <yves_guerin@groupevsc.com>
2019-10-23 09:26:38 +0200

    init

---
+++ precache-manifest.d5bf64c3d3cb10bd6c75a1d6690a0011.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134c63573cadb99dff62",
+    "url": "/css/app.70fa1f38.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "42dc172476f917c8d4963b9c4b2e7e89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8a5831f4e54ff48bc0d",
+    "url": "/js/about.e97b8d0b.js"
+  },
+  {
+    "revision": "134c63573cadb99dff62",
+    "url": "/js/app.98bc0054.js"
+  },
+  {
+    "revision": "18a8dff35c2ee226a844",
+    "url": "/js/chunk-vendors.d47cf30f.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
